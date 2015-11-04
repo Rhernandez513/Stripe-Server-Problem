0b3604a3b2f76bcd71c9e8806949796d55849408
Robert Hernandez <rhernandez513@gmail.com>
2015-11-04 13:22:51 -0600

    add array size function

--- app/include/main.h
+++ app/include/main.h
@@ -1,4 +1,14 @@
 #include "gtest/gtest.h"
 
+int next_server_number(int input[], std::size_t size);
+int next_server_number(int input[]);
 
-int next_server_number(int input[], size_t size);
+// Determines the size of an array similarly to 
+// int size = sizeof(input) / sizeof(int);
+// which fails for arrays passed as parameters
+// As the function siganture decays to 
+//
+// int next_server_number(int * input, size_t size);
+//
+template <typename T, std::size_t N>
+inline std::size_t arr_size(T const (&)[N]) { return N; }
